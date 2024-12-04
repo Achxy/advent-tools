91952ab86dde4d5d45d6e40e1853c440f5d4c968
Achyuth Jayadevan <achyuth@jayadevan.in>
2024-12-05 00:29:00 +0530

    impl `get`, `fetch`, `getch`

--- advent/_data_handle.py
+++ advent/_data_handle.py
@@ -1,18 +1,35 @@
+from _config_reader import (
+    CONFIGURATION,
+    Configuration,
+    UnformattedPath,
+    get_session_token,
+)
 from _downloader import Downloader
-from _config_reader import Configuration, UnformattedPath
 
-
-DOWNLOAD_PATH = Configuration.download_path
+from advent._helpers import as_sync
 
 
 def get(year: int, day: int) -> str | None:
-    fullpath = UnformattedPath.join_path(year=year, day=day)
+    fullpath = UnformattedPath.join_path(year, day, *CONFIGURATION.download_path())
+    print(fullpath)
     if fullpath.exists():
-        return fullpath.read_text()
+        return fullpath.read_text() or None
 
-def fetch(year: int, day: int):
-    fullpath = UnformattedPath.join_path(year=year, day=day)
-    
 
-def getch(year: int, day: int):
-    ...
+def fetch(year: int, day: int) -> str:
+    fullpath = UnformattedPath.join_path(year, day, *CONFIGURATION.download_path())
+    token = get_session_token()
+    content: str = Downloader(token).get_content_for_date(year, day)
+    fullpath.parent.mkdir(parents=True, exist_ok=True)
+    fullpath.write_text(content)
+    return content
+
+
+def getch(year: int, day: int) -> str:
+    if data := get(year, day):
+        return data
+    return fetch(year, day)
+
+
+def get_example_data(year: int, day: int):
+    return "..."
