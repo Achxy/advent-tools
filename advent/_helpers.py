6da96385862cdc31ca5fdbdc024b02eb5ac37669
Achyuth Jayadevan <achyuth@jayadevan.in>
2024-12-05 00:28:08 +0530

    helpers for the CLI impl

--- advent/_helpers.py
+++ advent/_helpers.py
@@ -1,10 +1,17 @@
-from asyncio import run
+from asyncio import get_running_loop, new_event_loop, run, set_event_loop
 from typing import Any, Coroutine, TypeVar
 
 _R = TypeVar("_R")
 
 
+def get_loop():
+    try:
+        get_running_loop()
+    except RuntimeError:
+        return new_event_loop()
+
+
 def as_sync(coro: Coroutine[Any, Any, _R]) -> _R:
     # Downloader has a async implementation for multi-downloads that can be made using CLI
     # However, our python interface program has a sync implementation
-    return run(coro, debug=False)
+    return run(coro, debug=False, loop_factory=new_event_loop)
